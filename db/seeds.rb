730a603ffa584054d07cfa01823c5adfa5f27168
Jason White <jason.white@shopify.com>
2014-02-27 17:12:18 -0500

    remove unneeded CDN station seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-json = ActiveSupport::JSON.decode(File.read('db/seeds/stations.json'))
-json.each do |a|
-  if Station.find_by(ec_identifier: a['id']).nil?
-    Station.create(ec_identifier: a['id'])
-  end
-end
