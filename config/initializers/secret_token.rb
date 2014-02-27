1b94543f946c36f337022f4be2de0ca40a2d1f73
Jason White <jason.white@shopify.com>
2014-02-27 10:42:01 -0500

    barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cumulus::Application.config.secret_key_base = 'a4dca7d9ab3973e5b26834641677fdf1ebfbd33279bd2761cfb3f2e1595cffa238c78b07ce2fed8d8147bd83f1ecd333a14a991b8ef8dd7f59287b8811f81c87'
